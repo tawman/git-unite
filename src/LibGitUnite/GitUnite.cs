399f962364304b67f0c3bcf2b8f8d9d47bbe0b9f
Todd A. Wood <todd.wood@woodcp.com>
2014-04-23 23:39:00 -0500

    Refactored host OS folder list to exclude .git directory

--- src/LibGitUnite/GitUnite.cs
+++ src/LibGitUnite/GitUnite.cs
@@ -19,30 +19,19 @@ namespace LibGitUnite
         /// <param name="dryrun">dry run without making changes</param>
         public static void Process(string gitPath, bool dryrun)
         {
-            // Build a list of directory names as seen by the host operating system
-            List<string> folders;
-            var path = new DirectoryInfo(gitPath).FullName;
+            var path = new DirectoryInfo(gitPath);
 
-            try
+            using (var repo = new UniteRepository(path.FullName))
             {
-                folders = Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories).ToList();
-            }
-            catch (Exception ex)
-            {
-                Console.Write("Git.Unite: ");
-                Console.WriteLine(ex.Message);
-                return;
-            }
+                // Build a list of directory names as seen by the host operating system
+                var folders = GetHostDirectoryInfo(path, repo.Info.Path);
+                if (!folders.Any()) return;
 
-            if (!folders.Any()) return;
-
-            using (var repo = new UniteRepository(path))
-            {
                 // Find all repository files with directory paths not found in the host OS folder collection
                 var indexEntries =
                     repo.Index.Where(f => f.Path.LastIndexOf(Separator, StringComparison.Ordinal) != -1
                                           &&
-                                          !folders.Any(s => s.Contains(f.Path.Substring(0, f.Path.LastIndexOf(Separator, StringComparison.Ordinal)))));
+                                          !folders.Any(s => s.FullName.Contains(f.Path.Substring(0, f.Path.LastIndexOf(Separator, StringComparison.Ordinal)))));
 
                 // Unite the casing of the repository file directory path with the casing seen by the host operating system
                 foreach (var entry in indexEntries)
@@ -52,8 +41,8 @@ namespace LibGitUnite
 
                     // Match host OS folder based on minimum length to find top level directory to target
                     var target = folders
-                                     .Where(x => x.ToLower().Contains(entry.Path.Substring(0, lastIndexOf).ToLower()))
-                                     .OrderBy(x => x.Length)
+                                     .Where(x => x.FullName.ToLower().Contains(entry.Path.Substring(0, lastIndexOf).ToLower()))
+                                     .OrderBy(x => x.FullName.Length)
                                      .FirstOrDefault() + Separator + filename;
 
                     var sourcePath = repo.Info.WorkingDirectory + entry.Path;
@@ -64,6 +53,28 @@ namespace LibGitUnite
                     else
                         repo.Unite(sourcePath, target);
                 }
+            }
+        }
+
+        /// <summary>
+        /// Get a list of directory names as seen by the host operating system
+        /// </summary>
+        /// <param name="path">Git path <see cref="DirectoryInfo"/></param>
+        /// <param name="gitRepoPath">Directory path to the repository .git directory</param>
+        /// <returns>A list of <see cref="DirectoryInfo"/> reported by host OS</returns>
+        private static List<DirectoryInfo> GetHostDirectoryInfo(DirectoryInfo path, string gitRepoPath)
+        {
+            try
+            {
+                return path.EnumerateDirectories("*", SearchOption.AllDirectories)
+                .Where(d => !d.FullName.ToLowerInvariant().StartsWith(gitRepoPath.TrimEnd('\\').ToLowerInvariant()))
+                .ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Git.Unite: ");
+                Console.WriteLine(ex.Message);
+                return new List<DirectoryInfo>();
             }
         }
     }
