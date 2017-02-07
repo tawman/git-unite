cff150ceee931bbc420cf4a3fa891cfa02ad179e
Todd A. Wood <twood@silencershop.com>
2017-02-07 11:15:28 -0600

    latest LibGit2Sharp no longer need dynamic method innvocation

--- src/LibGitUnite/UniteRepository.cs
+++ src/LibGitUnite/UniteRepository.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
 using LibGit2Sharp;
 
 namespace LibGitUnite
 {
     public class UniteRepository : IDisposable
     {
-        private readonly MethodInfo _prepareBatch;
-        private readonly MethodInfo _removeFromIndex;
-        private readonly MethodInfo _addToIndex;
-        private readonly MethodInfo _updatePhysicalIndex;
+        //private readonly MethodInfo _prepareBatch;
         private readonly GitUnite.OptionFlags _options;
-        public Repository GitRepository { get; private set; }
+        public Repository GitRepository { get; }
 
         /// <summary>
         /// Perform a dry run (--dry-run) only and report proposed changes
@@ -31,17 +26,6 @@ namespace LibGitUnite
         {
             GitRepository = new Repository(path);
             _options = options;
-
-            _prepareBatch = GitRepository.Index.GetType().GetMethod(
-                "PrepareBatch",
-                BindingFlags.NonPublic | BindingFlags.Instance,
-                null,
-                new[] { typeof(IEnumerable<string>), typeof(IEnumerable<string>) },
-                null);
-
-            _removeFromIndex = GitRepository.Index.GetType().GetMethod("RemoveFromIndex", BindingFlags.NonPublic | BindingFlags.Instance);
-            _addToIndex = GitRepository.Index.GetType().GetMethod("AddToIndex", BindingFlags.NonPublic | BindingFlags.Instance);
-            _updatePhysicalIndex = GitRepository.Index.GetType().GetMethod("UpdatePhysicalIndex", BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
         /// <summary>
@@ -51,51 +35,21 @@ namespace LibGitUnite
         /// <param name = "destinationPath">The target path of the file within the working directory.</param>
         public void Unite(string sourcePath, string destinationPath)
         {
-            Unite(new[] { sourcePath }, new[] { destinationPath });
-        }
-
-        /// <summary>
-        /// Modified version of LibGit2Sharp.Index.Move without the OS file system checks to unite file with proper case path
-        /// </summary>
-        /// <param name="sourcePaths">List containing path of each file within the working directory which has to be renamed.</param>
-        /// <param name="destinationPaths">List containing target path of each file within the working directory.</param>
-        /// ReSharper disable once MemberCanBePrivate.Global
-        public void Unite(IEnumerable<string> sourcePaths, IEnumerable<string> destinationPaths)
-        {
-            if (sourcePaths == null)
-                throw new ArgumentNullException("sourcePaths");
-
-            if (destinationPaths == null)
-                throw new ArgumentNullException("destinationPaths");
-
-            dynamic batch = _prepareBatch.Invoke(GitRepository.Index, new object[] { sourcePaths, destinationPaths });
-
-            if (batch.Count == 0)
-                throw new ArgumentNullException("sourcePaths");
-
-            foreach (var keyValuePair in batch)
+            if (DryRunOnly)
             {
-                var from = keyValuePair.Key.Item1;
-                var to = keyValuePair.Value.Item1;
+                Console.WriteLine("proposed rename: {0} -> {1}", sourcePath, destinationPath);
+                return;
+            }
 
-                if (DryRunOnly)
-                {
-                    Console.WriteLine("proposed rename: {0} -> {1}", from, to);
-                }
-                else
-                {
-                    try
-                    {
-                        _removeFromIndex.Invoke(GitRepository.Index, new object[] { from });
-                        _addToIndex.Invoke(GitRepository.Index, new object[] { to });
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("error changing: {0} -> {1} [{2}]", from, to, ex.Message);
-                    }
-
-                    _updatePhysicalIndex.Invoke(GitRepository.Index, new object[] { });
-                }
+            try
+            {
+                GitRepository.Index.Remove(sourcePath.Replace(GitRepository.Info.WorkingDirectory, string.Empty));
+                GitRepository.Index.Add(destinationPath.Replace(GitRepository.Info.WorkingDirectory, string.Empty));
+                GitRepository.Index.Write();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error changing: {0} -> {1} [{2}]", sourcePath, destinationPath, ex.Message);
             }
         }
 
