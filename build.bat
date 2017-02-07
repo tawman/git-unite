0dc16f795f3cc4b6c10eb860a8a6098f468214a3
Todd A. Wood <twood@silencershop.com>
2017-02-07 11:24:19 -0600

    added psake tooling to support msbuild with C# 6 support

--- build.bat
+++
@@ -1,2 +0,0 @@
-%systemroot%\Microsoft.NET\Framework\V4.0.30319\msbuild /t:clean /v:n /p:Configuration=DEBUG src\Git.Unite.sln
-%systemroot%\Microsoft.NET\Framework\V4.0.30319\msbuild /t:build /v:n /p:Configuration=DEBUG src\Git.Unite.sln
